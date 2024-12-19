0e9fd5d462963dc1d58ee8f4c808074d58727b77
Hifaris <nattapongbe@gmail.com>
2024-12-20 00:26:37 +0700

    create: service sub components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
+import Service from './components/Services/service'
+import Banner from './components/Banner/Banner'
+import Subscribe from './components/Subscribe/Subscribe'
+import Banner2 from './components/Banner/Banner2'
 
 function App() {
   return (
     <main className='overflow-x-hidden bg-white text-dark'>
-
       <Hero/>
+      <Service/>
+      <Banner/>
+      <Subscribe/>
+      <Banner2/>
     </main>
   )
 }
